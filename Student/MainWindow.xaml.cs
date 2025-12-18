15c110951d27bbc87f6db4201383b543ab8433a6
og_den1x <denisdor2005@gmail.com>
2025-12-18 19:45:36 +0900

    стиль

--- Student/MainWindow.xaml.cs
+++ Student/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Student.Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Student
 {
@@ -20,9 +12,77 @@ namespace Student
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Students> _students;
+        public ObservableCollection<Students> Students
+        {
+            get => _students;
+            set
+            {
+                _students = value;
+                OnPropertyChanged(nameof(Students));
+            }
+        }
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
+           
+            // Инициализация тестовых данных
+            InitializeTestData();
+        }
+        private void InitializeTestData()
+        {
+            Students = new ObservableCollection<Students>
+        {
+            new Students { LastName = "Иванов", FirstName = "Иван", MiddleName = "Иванович", Group = "ИТ-101" },
+            new Students { LastName = "Петров", FirstName = "Петр", MiddleName = "Петрович", Group = "ИТ-101" },
+            new Students { LastName = "Сидорова", FirstName = "Анна", MiddleName = "Сергеевна", Group = "ИТ-102" },
+            new Students { LastName = "Кузнецов", FirstName = "Алексей", MiddleName = "Владимирович", Group = "ИТ-101" },
+            new Students { LastName = "Смирнова", FirstName = "Елена", MiddleName = "Александровна", Group = "ИТ-103" },
+            new Students { LastName = "Васильев", FirstName = "Дмитрий", MiddleName = "Олегович", Group = "ИТ-102" },
+            new Students { LastName = "Павлова", FirstName = "Ольга", MiddleName = "Игоревна", Group = "ИТ-103" },
+            new Students { LastName = "Николаев", FirstName = "Сергей", MiddleName = "Михайлович", Group = "ИТ-101" },
+            new Students { LastName = "Федорова", FirstName = "Мария", MiddleName = "Дмитриевна", Group = "ИТ-102" },
+            new Students { LastName = "Морозов", FirstName = "Андрей", MiddleName = "Викторович", Group = "ИТ-103" },
+            new Students { LastName = "Волкова", FirstName = "Наталья", MiddleName = "Анатольевна", Group = "ИТ-101" },
+            new Students { LastName = "Алексеев", FirstName = "Владимир", MiddleName = "Сергеевич", Group = "ИТ-102" },
+            new Students { LastName = "Лебедева", FirstName = "Татьяна", MiddleName = "Владимировна", Group = "ИТ-103" },
+            new Students { LastName = "Семенов", FirstName = "Артем", MiddleName = "Ильич", Group = "ИТ-101" },
+            new Students { LastName = "Егорова", FirstName = "Кристина", MiddleName = "Павловна", Group = "ИТ-102" }
+        };
+        }
+        // Реализация INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MaximizeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Student/Model/Students.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student.Model
+{
+    public class Students : INotifyPropertyChanged
+    {
+        private bool _isSelected;
+        private string _lastName;
+        private string _firstName;
+        private string _middleName;
+        private string _group;
+
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set
+            {
+                _isSelected = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string LastName
+        {
+            get => _lastName;
+            set
+            {
+                _lastName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string FirstName
+        {
+            get => _firstName;
+            set
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string MiddleName
+        {
+            get => _middleName;
+            set
+            {
+                _middleName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Group
+        {
+            get => _group;
+            set
+            {
+                _group = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
